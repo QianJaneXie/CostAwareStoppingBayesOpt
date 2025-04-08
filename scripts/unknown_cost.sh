05aa861391ef7ad37912c6cc17ab89cdf3ec1b49
Qian Xie <90579251+JaneQianXie@users.noreply.github.com>
2025-04-08 12:00:58 -0400

    rename repo

---
+++ scripts/unknown_cost.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J unknown_cost                # Job name
+#SBATCH -o unknown_cost_%j.out         # Output file (%j expands to jobID)
+#SBATCH -e unknown_cost_%j.err         # Error log file (%j expands to jobID)
+#SBATCH --mail-type=ALL                      # Request status by email 
+#SBATCH --mail-user=qx66@cornell.edu         # Email address to send results to
+#SBATCH -N 1                                 # Total number of nodes requested
+#SBATCH -n 1                                 # Total number of cores requested
+#SBATCH --array=0-99                        # Number of jobs
+#SBATCH --mem-per-cpu=16G                  # Server memory requested (per node)
+#SBATCH -t 240:00:00                           # Time limit (hh:mm:ss)
+#SBATCH --partition=default_partition        # Request partition
+#SBATCH --ntasks-per-node=1                  # Number of tasks per node
+
+source /share/apps/anaconda3/2021.05/etc/profile.d/conda.sh
+conda activate automl_env
+wandb login
+wandb agent 'ziv-scully-group/PandoraBayesOpt/mz1uz9lk' --count 1
+conda deactivate
