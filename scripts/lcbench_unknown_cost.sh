848e8f0f0bc1131125f7c77ef15392ff84df7077
Qian Xie <90579251+JaneQianXie@users.noreply.github.com>
2025-05-01 19:27:54 -0400

    update scripts

--- scripts/lcbench_unknown_cost.sh
+++ scripts/lcbench_unknown_cost.sh
@@ -7,7 +7,7 @@
 #SBATCH -N 1                                 # Total number of nodes requested
 #SBATCH -n 1                                 # Total number of cores requested
 #SBATCH --array=0-99                        # Number of jobs
-#SBATCH --mem-per-cpu=16G                  # Server memory requested (per node)
+#SBATCH --mem-per-cpu=20G                  # Server memory requested (per node)
 #SBATCH -t 240:00:00                           # Time limit (hh:mm:ss)
 #SBATCH --partition=default_partition        # Request partition
 #SBATCH --ntasks-per-node=1                  # Number of tasks per node
@@ -15,5 +15,5 @@
 source /share/apps/anaconda3/2021.05/etc/profile.d/conda.sh
 conda activate automl_env
 wandb login
-wandb agent 'ziv-scully-group/PandoraBayesOpt/x4zvtxcb' --count 1
+wandb agent 'ziv-scully-group/PandoraBayesOpt/9kiuezks' --count 1
 conda deactivate
