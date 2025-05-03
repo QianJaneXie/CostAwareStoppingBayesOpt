d435cdff56ba280c775f7aad75c5d0ca7fec45a6
Qian Xie <90579251+JaneQianXie@users.noreply.github.com>
2025-05-03 17:03:18 -0400

    update fit_gp_model

--- pandora_automl/utils.py
+++ pandora_automl/utils.py
@@ -15,11 +15,10 @@ import math
 def fit_gp_model(
         X: Tensor, 
         objective_X: Tensor, 
-        cost_X: Optional[Tensor] = None, 
-        unknown_cost: bool = False, 
         kernel: Optional[torch.nn.Module] = None,
+        cost_X: Optional[Tensor] = None, 
+        unknown_cost: bool = False,
         gaussian_likelihood: bool = False,
-        noisy_observation: bool = False, 
         noise_level: float = 1e-6,
         output_standardize: bool = False,
     ):
@@ -40,22 +39,18 @@ def fit_gp_model(
 
     Y = torch.cat((objective_X, log_cost_X), dim=-1) if unknown_cost else objective_X
         
-    if noisy_observation:
-        likelihood = None
+    if gaussian_likelihood:
+        _, aug_batch_shape = SingleTaskGP.get_batch_dimensions(
+                train_X=X,
+                train_Y=Y,
+            )
+        likelihood = GaussianLikelihood(
+                batch_shape=aug_batch_shape,
+                noise_constraint=Interval(lower_bound=noise_level, upper_bound=10*noise_level),
+            )
     else:
-        if gaussian_likelihood:
-            _, aug_batch_shape = SingleTaskGP.get_batch_dimensions(
-                    train_X=X,
-                    train_Y=Y,
-                )
-            likelihood = GaussianLikelihood(
-                    batch_shape=aug_batch_shape,
-                    noise_constraint=Interval(lower_bound=noise_level, upper_bound=10*noise_level),
-                )
-        else:
-            Yvar = torch.ones(len(Y)) * noise_level
-            
-            likelihood = FixedNoiseGaussianLikelihood(noise=Yvar)
+        Yvar = torch.ones(len(Y)) * noise_level
+        likelihood = FixedNoiseGaussianLikelihood(noise=Yvar)
 
     # Outcome transform
     if output_standardize == True:
